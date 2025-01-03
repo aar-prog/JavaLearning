58c3a453da284eeadd0f7c7dd0e2e137a2468dbb
Aarush <aarush.pothula@gmail.com>
2025-01-03 17:08:39 +0100

    Switch challenge

--- src/SwitchChallenge.java
+++ src/SwitchChallenge.java
@@ -1,4 +1,26 @@
 public class SwitchChallenge {
     public static void main(String[] args) {
+        char charValue = 'X';
+
+        switch (charValue){
+            case 'A':
+                System.out.println("Able");
+                break;
+            case 'B':
+                System.out.println("Baker");
+                break;
+            case 'C':
+                System.out.println("Charlie");
+                break;
+            case 'D':
+                System.out.println("Dog");
+                break;
+            case 'E':
+                System.out.println("Easy");
+                break;
+            default:
+                System.out.println("The letter " + charValue + " is unknown");
+                break;
+        }
     }
 }
